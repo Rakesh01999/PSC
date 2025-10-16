498f6ca0d91a865ea7bc364d2acbbfa6ca80d356
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-16 11:24:50 +0600

    how many

---
+++ week01/B_How_many.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int S, T;cin >> S >> T;
+
+    int cnt = 0;
+    for (int a = 0; a <= S; ++a) {
+        for (int b = 0; b <= S - a; ++b) {
+            for (int c = 0; c <= S - a - b; ++c) {
+                if (a * b * c <= T) {
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    cout << cnt << '\n';
+
+
+    return 0;
+}
+
