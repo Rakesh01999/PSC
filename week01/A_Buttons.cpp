37af6c32ce0bfb3bfd24249775dfdb9334b60c8b
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-16 07:52:13 +0600

    updated with problem

---
+++ week01/A_Buttons.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;cin>>a>>b;
+    if( 2*a - 1 >= (a+b))
+        cout<<2*a - 1<<endl;
+    else
+        cout<<a+b<<endl;
+    
+    return 0;
+}
