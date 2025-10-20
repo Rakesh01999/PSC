631a6cd8f258f912f7bca61896f4768abde3fa50
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-20 23:15:23 +0600

    B. YetnotherrokenKeoard

--- week02/B_YetnotherrokenKeoard.cpp
+++ week02/B_YetnotherrokenKeoard.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <deque>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t; cin >> t;
     while (t--) {
         string input;cin >> input;
