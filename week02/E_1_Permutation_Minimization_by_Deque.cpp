c07bfb20f3c85bc66631e433c8fc8247ab083ac1
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-21 14:27:51 +0600

    E1. Permutation Minimization by Deque

--- week02/E_1_Permutation_Minimization_by_Deque.cpp
+++ week02/E_1_Permutation_Minimization_by_Deque.cpp
@@ -1,30 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include<bits/stdc++.h>
-using namespace std;    
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int main(){
-    int t;cin>>t;
-    while(t--){
+    int t;cin >> t;
+    while (t--) {
+        int n ; cin >> n;
 
-        int n; cin >> n;
-        deque<char> dq;
-        for(int i = 0; i < n; i++){
-            char c; cin >> c;
-            if(dq.empty()){
-                dq.push_back(c);
-            } else {
-                if(c <= dq.front()){
-                    dq.push_front(c);
-                } else {
-                    dq.push_back(c);
-                }
-            }
+        vector<int> p(n);
+        
+        for (int i = 0; i < n; i++) 
+            cin >> p[i];
+
+        deque<int> dq;
+        dq.push_back(p[0]);
+        
+        for (int i = 1; i < n; i++) {
+            if (p[i] < dq.front())
+                dq.push_front(p[i]);
+            else
+                dq.push_back(p[i]);
         }
-        for(char c : dq){
-            cout << c << " ";
-        }cout<<endl;
-    
+
+        for (int x : dq)
+            cout << x << " ";
+        cout << "\n";
     }
 
+    
     return 0;
 }
