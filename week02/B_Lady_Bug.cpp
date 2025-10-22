c51bab4e87cf828d84605045ca70626761bdab23
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-22 23:14:15 +0600

    B. Lady Bug - https://codeforces.com/problemset/problem/2092/B

--- week02/B_Lady_Bug.cpp
+++ week02/B_Lady_Bug.cpp
@@ -1,15 +1,56 @@
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;cin>>n;
-    string a,b;cin>>a>>b;
-    if(a.find('1')){
-        cout<<"YES"<<endl;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t ; cin >> t;
+    while (t--){
+        int n;cin >> n;
+        string s, ss;cin >> s >> ss;
+        string s1, s2;
+        for (int i = 0; i < n; i++)
+        {
+            if (i & 1)
+            {
+                s1.push_back(ss[i]);
+                s2.push_back(s[i]);
+            }
+            else
+            {
+                s1.push_back(s[i]);
+                s2.push_back(ss[i]);
+            }
+        }
+
+        int zeros1 = 0, zeros2 = 0;
+        for (int i = 0; i < s1.size(); i++)
+        {
+            if (s1[i] == '0')
+            {
+                zeros1++;
+            }
+        }
+        for (int i = 0; i < s2.size(); i++)
+        {
+            if (s2[i] == '0')
+            {
+                zeros2++;
+            }
+        }
+
+        if (zeros1 >= ceil(s1.size() / 2.0) && zeros2 >= (s2.size() / 2))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    else{
-        cout<<"NO"<<endl;
-    }
+
+    
     return 0;
 }
