feb59e754319fd2cee1caac50f7a4fd0d95fecdf
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-22 21:49:54 +0600

    updated

---
+++ week02/B_Lady_Bug.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    string a,b;cin>>a>>b;
+    if(a.find('1')){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
