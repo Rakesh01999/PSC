c243d3bdfba89f4ade2e67bb6a8229a4a0b68102
Rakesh Biswas <rbiswas01999@gmail.com>
2025-11-13 19:30:48 +0600

    Count_Occurences_of_Anagrams

--- week04/Count_Occurences_of_Anagrams.cpp
+++ week04/Count_Occurences_of_Anagrams.cpp
@@ -3,25 +3,24 @@ using namespace std;
 
 int countAnagrams(string txt, string pat) {
     int n = txt.size(), m = pat.size();
+    if (m > n) return 0;
 
     vector<int> freqPat(26, 0), freqWin(26, 0);
-    
-    for (char c : pat) 
-        freqPat[c - 'a']++;
+    for (char c : pat) freqPat[c - 'a']++;
 
     int count = 0;
 
+    // first window
     for (int i = 0; i < m; i++)
         freqWin[txt[i] - 'a']++;
 
-    if (freqWin == freqPat) 
-        count++;
+    if (freqWin == freqPat) count++;
 
+    // slide window
     for (int i = m; i < n; i++) {
-        freqWin[txt[i] - 'a']++;         
-        freqWin[txt[i - m] - 'a']--;     
-        if (freqWin == freqPat) 
-            count++;
+        freqWin[txt[i] - 'a']++;         // add new char
+        freqWin[txt[i - m] - 'a']--;     // remove old char
+        if (freqWin == freqPat) count++;
     }
 
     return count;
---
+++ week04/test.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int n;cin>>n;
+    // string s;cin>>s;
+    // vector<int>v(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     v[s[i]-'a']++;        
+    // }
+    
+    // for(auto val:v){
+    //     cout<<val<<endl;
+    // }
+
+    cout<<5+'a'<<endl;
+
+    return 0;   
+}
