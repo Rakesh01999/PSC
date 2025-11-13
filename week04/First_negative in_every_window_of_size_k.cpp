bf3ccb2d07d75e836ba11b05a2db671b13e061e1
Rakesh Biswas <rbiswas01999@gmail.com>
2025-11-13 19:44:11 +0600

    First_negative in_every_window_of_size_k

---
+++ week04/First_negative in_every_window_of_size_k.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[], long long int n, long long int k) {
+    deque<long long int> dq;
+    vector<long long> result;
+
+    for (int i = 0; i < k; i++) {
+        if (arr[i] < 0)
+            dq.push_back(i);
+    }
+
+    for (int i = k; i < n; i++) {
+        if (!dq.empty())
+            result.push_back(arr[dq.front()]);
+        else
+            result.push_back(0);
+
+        while (!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+
+        if (arr[i] < 0)
+            dq.push_back(i);
+    }
+
+    if (!dq.empty())
+        result.push_back(arr[dq.front()]);
+    else
+        result.push_back(0);
+
+    return result;
+}
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+    long long arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+    for (auto x : ans) cout << x << " ";
+    return 0;
+}
