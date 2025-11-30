bbd98139dd22cb1c5caccd423317a9d3adb70f0a
Rakesh Biswas <rbiswas01999@gmail.com>
2025-11-30 21:50:46 +0600

    Dull Operation

---
+++ week07/Dull_Operation.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t ;cin >> t;
+
+    while(t--) {
+        ll n;cin >> n;
+
+        ll x = n;
+        
+        ll y = n - 1;
+
+        cout << x << " " << y << "\n";
+    }
+
+    
+    return 0;
+}
+
