91fff6f9c6a428b7500a35b241ff8641471cbcb5
Rakesh Biswas <rbiswas01999@gmail.com>
2025-12-05 18:56:48 +0600

    updated

--- week07/Longest_AND_Subarray.cpp
+++ week07/Longest_AND_Subarray.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T;
-    cin >> T;
-    while (T--) {
-        long long N;
-        cin >> N;
+    int t;cin >> t;
 
-        // largest power of 2 <= N
-        long long p = 1;
-        while (p * 2 <= N) p *= 2;
+    while(t--) {
 
-        long long ans = max(N - p + 1, p / 2);
+        ll n;cin >> n;
+
+        // largest power of 2 <= n
+        ll p = 1;
+        while (p * 2 <= n) 
+            p *= 2;
+
+        ll ans = max(n - p + 1, p / 2);
+        
         cout << ans << "\n";
     }
+
     
     return 0;
 }
