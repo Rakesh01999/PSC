a223ec5ecf8d216514115c284a1a4bcbbbc775ab
Rakesh Biswas <rbiswas01999@gmail.com>
2025-12-18 19:35:03 +0600

    Interesting Drink

---
+++ week08/B_Interesting_drink.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;cin >> n;
+    vector<int> x(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+
+    sort(x.begin(), x.end());
+
+    int q;cin >> q;
+
+    while(q--) {
+        ll m;cin >> m;
+
+        int count = upper_bound(x.begin(), x.end(), m) - x.begin();
+
+        cout << count << "\n";
+
+    }
+
+
+    return 0;
+}
