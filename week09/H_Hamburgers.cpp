1be9184bb67080e17148f655a1df5af6a7db794b
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-11 22:41:49 +0600

    Hamburgers doc updated

--- week09/H_Hamburgers.cpp
+++ week09/H_Hamburgers.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+#define ll long long
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string recipe;
-    cin >> recipe;
+    string recipe;cin >> recipe;
 
     ll nb, ns, nc;cin >> nb >> ns >> nc;
 
@@ -16,31 +15,34 @@ int main() {
     ll r;cin >> r;
 
     ll needB = 0, needS = 0, needC = 0;
-    for (char ch : recipe) {
-        if (ch == 'B') needB++;
-        else if (ch == 'S') needS++;
-        else if (ch == 'C') needC++;
+    
+    for (char c : recipe) {
+        if (c == 'B') needB++;
+        else if (c == 'S') needS++;
+        else if (c == 'C') needC++;
     }
 
-    auto canMake = [&](ll k) {
-        ll bread = max(0LL, needB * k - nb);
-        ll sausage = max(0LL, needS * k - ns);
-        ll cheese = max(0LL, needC * k - nc);
-        ll cost = bread * pb + sausage * ps + cheese * pc;
-        return cost <= r;
-    };
+    ll low = 0, high = 1e13, ans = 0;
 
-    ll lo = 0, hi = 1e13, ans = 0;
-    while (lo <= hi) {
-        ll mid = (lo + hi) / 2;
-        if (canMake(mid)) {
+    while (low <= high) {
+        ll mid = (low + high) / 2;
+
+        ll buyB = max(0LL, needB * mid - nb);
+        ll buyS = max(0LL, needS * mid - ns);
+        ll buyC = max(0LL, needC * mid - nc);
+
+        ll cost = buyB * pb + buyS * ps + buyC * pc;
+
+        if (cost <= r) {
             ans = mid;
-            lo = mid + 1;
+            low = mid + 1;
         } else {
-            hi = mid - 1;
+            high = mid - 1;
         }
     }
 
     cout << ans << "\n";
+    
+    
     return 0;
 }
