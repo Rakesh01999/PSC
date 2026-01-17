0754c35a953799fde4115926ee3916b132a909b4
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-17 14:05:38 +0600

    C. Cows in Stalls - code updated

--- week09/C_Cows_in_Stalls.cpp
+++ week09/C_Cows_in_Stalls.cpp
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, k;cin >> n >> k;
+    int n, k;
+    cin >> n >> k;
 
-    vector<ll> stalls(n);
+    vector<long long> stalls(n);
     for (int i = 0; i < n; i++) {
         cin >> stalls[i];
     }
 
-    ll low = 0, high = stalls[n - 1] - stalls[0],answer = 0;
+    long long low = 0;
+    long long high = stalls[n - 1] - stalls[0];
+    long long answer = 0;
 
+    // Binary search on distance
     while (low <= high) {
+        long long mid = (low + high) / 2;
 
-        ll mid = (low + high) / 2;
-
-        ll lastPos = stalls[0];
-
+        // Check if we can place k cows with at least mid distance
+        long long lastPos = stalls[0];
         int cowsPlaced = 1;
 
         for (int i = 1; i < n; i++) {
@@ -31,13 +34,12 @@ int main() {
 
         if (cowsPlaced >= k) {
             answer = mid;
-            low = mid + 1;   
+            low = mid + 1;   // try for bigger distance
         } else {
-            high = mid - 1;  
+            high = mid - 1;  // reduce distance
         }
     }
 
     cout << answer << "\n";
-    
     return 0;
 }
