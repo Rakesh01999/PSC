3d36c9a1309935bbfea3fba308c353ea3c0316b3
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-18 11:07:30 +0600

    Factory Machines-code updated

--- week09/Factory_Machines.cpp
+++ week09/Factory_Machines.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    ll n, t;cin >> n >> t;
+    int n;
+    long long t;
+    cin >> n >> t;
 
-    vector<ll> k(n);
-
+    vector<long long> k(n);
     for (int i = 0; i < n; i++) {
         cin >> k[i];
     }
 
-    ll low = 0, high = *min_element(k.begin(), k.end()) * t, answer = high;
+    long long low = 0;
+    long long high = *min_element(k.begin(), k.end()) * t;
+    long long answer = high;
 
     while (low <= high) {
-        ll mid = (low + high) / 2;
+        long long mid = (low + high) / 2;
 
-        ll products = 0;
-        for (ll time : k) {
+        long long products = 0;
+        for (long long time : k) {
             products += mid / time;
-            if (products >= t)
-                break;
+            if (products >= t) break;  // avoid overflow
         }
 
         if (products >= t) {
             answer = mid;
-            high = mid - 1;  
+            high = mid - 1;  // try smaller time
         } else {
-            low = mid + 1;   
+            low = mid + 1;   // need more time
         }
     }
 
     cout << answer << "\n";
-
-    
     return 0;
 }
